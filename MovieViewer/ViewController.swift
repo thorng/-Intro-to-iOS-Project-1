3c71a2e1e5669919eb194b628c99935ec4f0a8fd
Timothy Horng <thorng@asu.edu>
2016-01-18 23:21:13 -0700

    Initial Commit

---
+++ MovieViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieViewer
+//
+//  Created by Timothy Horng on 1/18/16.
+//  Copyright © 2016 Timothy Horng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
